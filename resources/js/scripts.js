3c8d3e6f5dc5501516352c817164d9eb5ca4b4a1
Philip Waern <waernph@gmail.com>
2026-03-05 19:16:20 +0100

    Started on AudioContext

--- resources/js/scripts.js
+++ resources/js/scripts.js
@@ -1,1 +1,10 @@
 const context = new AudioContext();
+
+function playSound() {
+    const oscillator = context.createOscillator();
+    oscillator.type = "square";
+    oscillator.frequency(440, context.currentTime);
+    oscillator.connect(context.destination);
+    oscillator.start();
+}
+playSound();
